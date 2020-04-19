7a5c6f5275463dc8d48cf3278cd3454a1372e812
dev-song <52951039+dev-song@users.noreply.github.com>
2020-04-19 22:01:51 +0900

    Update script.js
    - Link hue to line color for displaying continuous color-change
    - Set direction for limiting lineWidth within certain range

--- projects/JavaScript30/Day 08/tutorial-Fun-with-HTML5-Canvas/script.js
+++ projects/JavaScript30/Day 08/tutorial-Fun-with-HTML5-Canvas/script.js
@@ -22,6 +22,7 @@ let direction = true;
 function draw(e) {
     if(!isDrawing) return;      // stop the function when isDrawing is false
 
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);   // starting point of the line
     ctx.lineTo(e.offsetX, e.offsetY);   // end point of the line
@@ -29,6 +30,22 @@ function draw(e) {
 
     // update lastX, lastY
     [lastX, lastY] = [e.offsetX, e.offsetY];
+
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+
+    // reverse value-changing direction when lineWidth reaches certain number
+    if (ctx.lineWidth > 100 || ctx.lineWidth < 10) {
+        direction = !direction;
+    }
+
+    if (direction) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
 }
 
 // isDrawing depends on various mouse events
