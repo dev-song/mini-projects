fbae7051f956e15a24428ae0e49b80a6f48c5bf4
dev-song <52951039+dev-song@users.noreply.github.com>
2020-04-21 23:51:54 +0900

    Update script.js
    Add my version of solution

--- projects/JavaScript30/Day 10/tutorial-Hold-Shift-and-Check-Checkboxes/script.js
+++ projects/JavaScript30/Day 10/tutorial-Hold-Shift-and-Check-Checkboxes/script.js
@@ -1,17 +1,37 @@
 let lastChecked;
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkboxesNodeList = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkboxes = Array.from(checkboxesNodeList);
+console.log(checkboxes);
 
 function handleCheck(e) {
-    let inBetween = false;
+    /* Tutorial Solution */
+
+    // let inBetween = false;
     
-    // Check if the shift key is down && the input is checked
-    if(e.shiftKey && this.checked) {
-        checkboxes.forEach(checkbox => {
-            if (checkbox === this || checkbox === lastChecked) {
-                inBetween = !inBetween;
-            }
-            if (inBetween) {
-                checkbox.checked = true;
+    // // Check if the shift key is down && the input is checked
+    // if(e.shiftKey && this.checked) {
+    //     checkboxes.forEach(checkbox => {
+    //         if (checkbox === this || checkbox === lastChecked) {
+    //             inBetween = !inBetween;
+    //         }
+    //         if (inBetween) {
+    //             checkbox.checked = true;
+    //         }
+    //     })
+    // }
+
+
+    
+    /* My Solution */
+    if (e.shiftKey && lastChecked) {
+        const former = checkboxes.indexOf(lastChecked);
+        const latter = checkboxes.indexOf(this);
+
+        checkboxes.forEach((checkbox, index) => {
+            if (former < latter) {
+                checkbox.checked = index >= former && index <= latter ? true : false; 
+            } else {
+                checkbox.checked = index <= former && index >= latter ? true : false;
             }
         })
     }
