789439b46e7fe3520a0b0480cda88757707460a3
dev-song <52951039+dev-song@users.noreply.github.com>
2020-04-22 15:29:38 +0900

    Update script.js
    Create variables of HTML elements

---
+++ projects/JavaScript30/Day 11/tutorial-Custom-Video-Player/scripts.js
@@ -0,0 +1,8 @@
+// Get elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelectorAll('[data-skip]');
+const ranges = document.querySelector('.player__slider');
