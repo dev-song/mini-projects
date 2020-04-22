e314ae92235114b271a9f3debc5cafc4fbc85bce
dev-song <52951039+dev-song@users.noreply.github.com>
2020-04-22 15:48:02 +0900

    Update script.js
    - Add updateButton() for changing button shape
    - Link updateButton on video play/pause

--- projects/JavaScript30/Day 11/tutorial-Custom-Video-Player/scripts.js
+++ projects/JavaScript30/Day 11/tutorial-Custom-Video-Player/scripts.js
@@ -12,6 +12,13 @@ function togglePlay() {
     video.paused ? video.play() : video.pause();
 }
 
+function updateButton() {
+    toggle.textContent = this.paused ? '▶' : '❚ ❚';
+}
+
 // Hook up event listeners
 video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
 toggle.addEventListener('click', togglePlay);
