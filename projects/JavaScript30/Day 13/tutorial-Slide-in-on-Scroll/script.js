f3b13202b188bd5db2829dc8b43b51f4547bfe80
dev-song <52951039+dev-song@users.noreply.github.com>
2020-04-25 23:46:27 +0900

    Update script.js
    - Add checkSlide for detecting scrolled length and adding and removing class automatically
    - Link checkSlide with debounce to window scroll event

--- projects/JavaScript30/Day 13/tutorial-Slide-in-on-Scroll/script.js
+++ projects/JavaScript30/Day 13/tutorial-Slide-in-on-Scroll/script.js
@@ -1,10 +1,11 @@
+// Reduce function execution to 20ms interval
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
         var context = this, args = arguments;
         var later = function() {
-        timeout = null;
-        if (!immediate) func.apply(context, args);
+            timeout = null;
+            if (!immediate) func.apply(context, args);
         };
         var callNow = immediate && !timeout;
         clearTimeout(timeout);
@@ -12,3 +13,25 @@ function debounce(func, wait = 20, immediate = true) {
         if (callNow) func.apply(context, args);
     };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide() {
+    sliderImages.forEach(img => {
+        // half point of the img
+        const slideInAt = window.scrollY + window.innerHeight - img.height / 2;
+        // bottom of the img
+        const imgBottom = img.offsetTop + img.height;
+        
+        const isHalfShown = slideInAt > img.offsetTop;
+        const isNotScrolledPast = window.scrollY < imgBottom;
+
+        if (isHalfShown && isNotScrolledPast) {
+            img.classList.add('active');
+        } else {
+            img.classList.remove('active');
+        }
+    });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
