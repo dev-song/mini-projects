116cf8895f85d467f93561104a47909091c95183
dev-song <52951039+dev-song@users.noreply.github.com>
2020-04-27 17:15:41 +0900

    Update script.js
    - Add addItem() for pushing the value inside the form into items array
    - Link addItem to submit event

--- projects/JavaScript30/Day 15/tutorial-LocalStorage/script.js
+++ projects/JavaScript30/Day 15/tutorial-LocalStorage/script.js
@@ -1,3 +1,25 @@
 const addItems = document.querySelector('.add-items');
 const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+    // stop page from reloading
+        // by default, form reloads the page or send the data to external source
+    e.preventDefault();
+
+    // 'this' refers 'form' elm
+        // it narrows down where to search
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        // in ES6, writing key-only in object means key and value are same
+        text,
+        done: false
+    };
+
+    items.push(item);
+    // 'this' refers 'form' elm, and form elm has reset method
+    this.reset();
+}
+
+// 'submit' event listener not only mouse input, but also keyboard input
+addItems.addEventListener('submit', addItem);
