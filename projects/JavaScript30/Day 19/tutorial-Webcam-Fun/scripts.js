ad4e15eb893d24d764b4b7b874c9db77aed691df
dev-song <52951039+dev-song@users.noreply.github.com>
2020-05-02 23:02:53 +0900

    Update scripts.js
    Add getVideo() for showing webcam on page's video element

--- projects/JavaScript30/Day 19/tutorial-Webcam-Fun/scripts.js
+++ projects/JavaScript30/Day 19/tutorial-Webcam-Fun/scripts.js
@@ -13,6 +13,14 @@ function getVideo() {
     navigator.mediaDevices.getUserMedia({video: true, audio: false})
         .then(localMediaStream => {
             console.log(localMediaStream);
+            // localMediaStream should be in a form of URL to make video work
+                // URL-form localMediaStream can be checked at video tag's src attribute
+            video.src = window.URL.createObjectURL(localMediaStream);
+            // without play(), only snapshots of the video are shown
+            video.play();
+        })
+        .catch(err => {
+            console.error(`Oh no!`, err);
         });
 }
 
