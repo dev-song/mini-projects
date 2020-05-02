adb7a0473a57be8873234292b6cbde8ea11d8a1c
dev-song <52951039+dev-song@users.noreply.github.com>
2020-05-02 22:56:49 +0900

    Add node modules for server
    Add provided node modules for server to deal with security issue*
    * Webcam can't be loaded without https or localhost

--- projects/JavaScript30/Day 19/tutorial-Webcam-Fun/scripts.js
+++ projects/JavaScript30/Day 19/tutorial-Webcam-Fun/scripts.js
@@ -1,5 +1,19 @@
+// because of security reason*, app needs a server
+    // * webcam can't be loaded without https or localhost
+
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+// get video piped into the video element
+function getVideo() {
+    // getUserMedia() returns Promise
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+        });
+}
+
+getVideo();
