4948b2355f81a38d6ea6281d17127ded16b647df
dev-song <52951039+dev-song@users.noreply.github.com>
2020-07-08 23:51:46 +0900

    Create NatureFooter component for displaying footer

---
+++ projects/website/nature/src/components/NatureFooter.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+
+import BrandInfo from "./BrandInfo";
+
+function NatureFooter(props) {
+  const data = {
+    background: props.background || "trnasparent",
+  };
+
+  const Footer = styled.footer`
+    width: 100%;
+    background: ${data.background};
+  `;
+
+  const FooterContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    max-width: 1200px;
+    margin: auto;
+    padding: 1rem 2rem;
+  `;
+
+  const ContactInfo = styled.div`
+    margin: 0 0.5rem;
+  `;
+
+  const EmailAddress = styled.h4`
+    margin: 0;
+  `;
+
+  const SocialNetwork = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `;
+
+  return (
+    <Footer>
+      <FooterContainer>
+        <BrandInfo />
+        <ContactInfo>
+          <EmailAddress>owner_email@gmail.com</EmailAddress>
+          <SocialNetwork></SocialNetwork>
+        </ContactInfo>
+      </FooterContainer>
+    </Footer>
+  );
+}
+
+export default NatureFooter;
