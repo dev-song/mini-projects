67084df4d84baacd00705a4e8f20b79538112057
dev_song <52951039+dev-song@users.noreply.github.com>
2020-07-09 17:58:50 +0900

    Update App.js
    Remove default React boilerplate
    Add NatureMain component to App.js

--- projects/website/nature/src/App.js
+++ projects/website/nature/src/App.js
@@ -1,30 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 import NatureHeader from "./components/NatureHeader";
-import Carousel from "./components/Carousel";
+import NatureMain from "./components/NatureMain";
 import NatureFooter from "./components/NatureFooter";
 
 function App() {
   return (
     <div className='App'>
       <NatureHeader />
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <Carousel />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <NatureMain />
       <NatureFooter />
     </div>
   );
