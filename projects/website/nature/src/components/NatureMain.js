f1e078ac026cfb4900adc7c86b2599f675f7485f
dev_song <52951039+dev-song@users.noreply.github.com>
2020-07-09 17:58:29 +0900

    Create NatureMain component

---
+++ projects/website/nature/src/components/NatureMain.js
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+
+import Carousel from "./Carousel";
+
+const Main = styled.main`
+  width: 100%;
+`;
+
+const Section = styled.section`
+  max-width: 1200px;
+  margin: auto;
+  padding: 4rem 2rem;
+`;
+
+class NatureMain extends React.Component {
+  state = {};
+
+  render () {
+    return (
+      <Main>
+        <Section className="intro-images">
+          <Carousel />
+        </Section>
+        <Section className="intro-message">
+          <h2>휴양림의 매력에 빠져보세요!</h2>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, vitae?</p>
+        </Section>
+      </Main>
+    )
+  }
+}
+
+export default NatureMain;
