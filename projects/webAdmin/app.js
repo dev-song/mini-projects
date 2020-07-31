3ca259c6d04ecbdf4b0b23324735b37529ac03e5
dev-song <52951039+dev-song@users.noreply.github.com>
2020-07-31 23:38:18 +0900

    Feat: add a router for validating login information

--- projects/webAdmin/app.js
+++ projects/webAdmin/app.js
@@ -1,24 +1,37 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const options = {
+  root: __dirname
+};
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
-  res.sendFile('index.html');
+  res.sendFile('index.html', options);
 })
 
 app.get('/admin', (req, res) => {
-  const options = {
-    root: __dirname
-  }
-  res.sendFile('admin.html', { root: __dirname });
+  res.sendFile('admin.html', options);
 })
 
 app.post('/validate-login', (req, res) => {
   const { id, pw } = req.body;
-  console.log(id, pw);
+  const validId = 'devsong';
+  const validPw = 'SecTest1';
+
+  if (id !== validId) {
+    console.log(`${id}는 존재하지 않는 ID입니다.`);
+    res.redirect('/admin');
+    return;
+  }
+  if (pw !== validPw) {
+    console.log(`${pw}는 올바르지 않은 비밀번호입니다.`);
+    res.redirect('/admin');
+    return;
+  }
+  res.send('로그인 성공');
 })
 
 app.listen(port, () => console.log(`Port ${port} is listening!`));
